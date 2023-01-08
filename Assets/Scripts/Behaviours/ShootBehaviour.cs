81cdc77a630514d5c65812f8e6c03db3eb44e987
ShiroKatake <tgkhang262@gmail.com>
2023-01-08 14:31:06 +1100

    fix(Shoot): moved shoot behaviour code to ShootBehaviour script

--- Assets/Scripts/Behaviours/ShootBehaviour.cs
+++ Assets/Scripts/Behaviours/ShootBehaviour.cs
@@ -6,15 +6,35 @@ namespace ShootSystem
     {
         private AmmoMagazine _ammoMagazine;
         private AmmoLoadout _ammoLoadout;
-        private Shoot _shoot;
 
-        public Shoot Shoot { get => _shoot; }
+        private AmmoType _loadedAmmoType;
 
         private void Awake()
         {
             _ammoMagazine = GetComponent<AmmoMagazineBehaviour>().AmmoMagazine;
             _ammoLoadout = GetComponent <AmmoLoadoutBehaviour>().AmmoLoadout;
-            _shoot = new Shoot(_ammoMagazine, _ammoLoadout);
+        }
+
+        public void LoadAmmo(int loadoutIndex)
+        {
+            _loadedAmmoType = _ammoLoadout.GetAmmo(loadoutIndex);
+            Debug.Log("Loaded");
+        }
+
+        public void ShootAmmo(int loadedAmmoLoadoutIndex)
+        {
+            // You can press down Ability2 then Ability1, then release Ability2
+            // and fire ammo1 without checking loadedAmmoLoadoutIndex
+            if (_loadedAmmoType == null || _loadedAmmoType.loadoutIndex != loadedAmmoLoadoutIndex) return;
+
+            if (!_ammoMagazine.TryConsumeAmmo(_loadedAmmoType.ammoCost))
+            {
+                Debug.Log("Whiff");
+                return;
+            }
+
+            Debug.Log($"Releasing {_loadedAmmoType.ammoCost}");
+            _loadedAmmoType = null;
         }
     }
 }
--- Assets/Scripts/InputMapping/InputManager.cs
+++ Assets/Scripts/InputMapping/InputManager.cs
@@ -20,12 +20,12 @@ public class InputManager: MonoBehaviour
 		_playerInput.Move.performed += context => _characterMovement.MoveDirection = context.ReadValue<Vector2>();
 		_playerInput.Move.canceled += _ => _characterMovement.MoveDirection = Vector2.zero;
 
-		_playerInput.Ability1.performed += _ => _shootBehaviour.Shoot.LoadAmmo(0);
-		_playerInput.Ability2.performed += _ => _shootBehaviour.Shoot.LoadAmmo(1);
-		_playerInput.Ability3.performed += _ => _shootBehaviour.Shoot.LoadAmmo(2);
+		_playerInput.Ability1.performed += _ => _shootBehaviour.LoadAmmo(0);
+		_playerInput.Ability2.performed += _ => _shootBehaviour.LoadAmmo(1);
+		_playerInput.Ability3.performed += _ => _shootBehaviour.LoadAmmo(2);
 
-		_playerInput.Ability1.canceled += _ => _shootBehaviour.Shoot.ShootAmmo(0);
-		_playerInput.Ability2.canceled += _ => _shootBehaviour.Shoot.ShootAmmo(1);
-		_playerInput.Ability3.canceled += _ => _shootBehaviour.Shoot.ShootAmmo(2);
+		_playerInput.Ability1.canceled += _ => _shootBehaviour.ShootAmmo(0);
+		_playerInput.Ability2.canceled += _ => _shootBehaviour.ShootAmmo(1);
+		_playerInput.Ability3.canceled += _ => _shootBehaviour.ShootAmmo(2);
 	}
 }
