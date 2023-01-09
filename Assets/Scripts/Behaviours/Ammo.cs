0a75f7fe1955190c9cc005786fd7021ff69b6be1
ShiroKatake <tgkhang262@gmail.com>
2023-01-09 13:52:49 +1100

    feat(AmmoType): added lifetime and sprite fields

--- Assets/Scripts/Behaviours/Ammo.cs
+++ Assets/Scripts/Behaviours/Ammo.cs
@@ -1,16 +1,54 @@
+using System;
 using UnityEngine;
+using TimerSystem;
 
 namespace ShootSystem
 {
-    public class Ammo : MonoBehaviour
+	public class Ammo : MonoBehaviour
     {
         [SerializeField] private AmmoType _ammoType;
 
-        public AmmoType AmmoType => _ammoType;
+		private SpriteRenderer _spriteRenderer;
+		private Countdown _lifetimeTimer;
+		private Action<Ammo> _onAmmoDie;
+
+		public AmmoType AmmoType
+		{
+			get => _ammoType;
+			set => _ammoType = value;
+		}
+
+		private void Awake()
+		{
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+			_lifetimeTimer = new Countdown(0, OnCountdownEnd);
+		}
+
+		public void OnAmmoDie(Action<Ammo> killAction)
+		{
+			_onAmmoDie = killAction;
+		}
+
+		private void OnEnable()
+		{
+			_spriteRenderer.sprite = _ammoType.sprite;
+			_lifetimeTimer.SetNewDuration(_ammoType.lifetimeSeconds);
+		}
+
+		private void Update()
+		{
+			_lifetimeTimer.Tick(Time.deltaTime);
+		}
+
+		private void OnCountdownEnd()
+		{
+			_onAmmoDie(this);
+			_lifetimeTimer.Reset();
+		}
 
 		private void OnTriggerEnter2D(Collider2D collision)
 		{
-			// Bring back to pool
+			_onAmmoDie(this);
 		}
 	}
 }
--- Assets/Scripts/ScriptableObjects/AmmoType/AmmoType.cs
+++ Assets/Scripts/ScriptableObjects/AmmoType/AmmoType.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Ammo Type")]
 public class AmmoType : ScriptableObject
 {
+    public Sprite sprite;
     public int ammoCost;
     public int damage;
+    public float lifetimeSeconds;
     public int loadoutIndex;
 }
